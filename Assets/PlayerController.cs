f580b96b2bd67edc136bf2bf05f33e7d0eb22702
Mioriarty <moritz.seppelt@googlemail.com>
2022-03-26 21:47:44 +0100

    + add library and charakter movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,17 +8,22 @@ public class PlayerController : MonoBehaviour
     private float speed = 8f; 
 
     private Rigidbody2D rb;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 inputVec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        this.rb.velocity = inputVec * speed;
+        this.rb.velocity = inputVec.normalized * speed;
+
+        this.animator.SetBool("isMoving", inputVec.sqrMagnitude > 0.001f);
+        this.animator.SetFloat("angle", Vector2.SignedAngle(inputVec, Vector2.up));
     }
 }
