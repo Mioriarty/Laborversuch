4ef0756ab2659b1a6253ccaa12b30708b58b0820
Mioriarty <moritz.seppelt@googlemail.com>
2022-03-30 12:31:52 +0200

    Add dashing

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float speed = 8f; 
+    [SerializeField] private float speed = 8f; 
+    [SerializeField] private float dashSpeed = 15f;
+    [SerializeField] private float dashDuration = 0.3f;
+    private float dashStartTime = -10f;
+    private Vector2 dashDirection;
 
     private Rigidbody2D rb;
     private Animator animator;
@@ -20,10 +23,26 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 inputVec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        this.rb.velocity = inputVec.normalized * speed;
+        bool isDashing = dashStartTime + dashDuration >= Time.time;
+        Vector2 desiredMovementDirection = Vector3.zero;
+        Vector2 inputVec = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
 
-        this.animator.SetBool("isMoving", inputVec.sqrMagnitude > 0.001f);
-        this.animator.SetFloat("angle", Vector2.SignedAngle(inputVec, Vector2.up));
+        if(!isDashing && inputVec != Vector2.zero && Input.GetKeyDown(KeyCode.Space)) {
+            dashStartTime = Time.time;
+            isDashing = true;
+            dashDirection = inputVec;
+        }
+
+        if(isDashing) {
+            this.rb.velocity = dashDirection * dashSpeed;
+            desiredMovementDirection = dashDirection;
+        } else {
+            this.rb.velocity = inputVec * speed;
+            desiredMovementDirection = inputVec;
+        }
+        
+
+        this.animator.SetBool("isMoving", desiredMovementDirection.sqrMagnitude > 0.001f);
+        this.animator.SetFloat("angle", Vector2.SignedAngle(desiredMovementDirection, Vector2.up));
     }
 }
